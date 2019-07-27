2e9b4ea50956b74d9fe517386e7946be9ed8d9f9
YUval Zarad <yuvalzarad@gmail.com>
2019-07-27 20:11:47 +0300

    added flask

--- server.py
+++ server.py
@@ -1,5 +1,13 @@
 from app.views import parse_images
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/group')
+def group_route():
+    return parse_images([r'C:/temp/images/family.jpg', 'C:/temp/images/family_girls.jpg'])
+
 
 if __name__ == "__main__":
-    res = parse_images([r'C:/temp/images/family.jpg', 'C:/temp/images/family_girls.jpg'])
-    print res
+    app.run(port=8080)
